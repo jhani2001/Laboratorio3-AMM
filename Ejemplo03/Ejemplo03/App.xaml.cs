41a3615837c1e36867b81bbb7e8f05ec7e62eb26
jhani2001 <jhani2001.alarcon@gmail.com>
2023-04-06 23:41:24 -0500

    first commit

---
+++ Ejemplo03/Ejemplo03/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejemplo03
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //comentario
+            //comentario2
+            MainPage = new StackLayoutCode();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Ejemplo03/Ejemplo03/StackLayoutCode.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejemplo03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StackLayoutCode : ContentPage
+    {
+        public StackLayoutCode()
+        {
+            InitializeComponent();
+            StackLayout stack = new StackLayout();
+            Button button = new Button
+            {
+                Text = "Demo",
+                BackgroundColor = Color.Red
+            };
+            Button button2 = new Button
+            {
+                Text = "Demo",
+                BackgroundColor = Color.Orange
+            };
+
+            stack.Children.Add(button);
+            stack.Children.Add(button2);
+
+
+            this.Content = stack;
+
+        }
+    }
+}
